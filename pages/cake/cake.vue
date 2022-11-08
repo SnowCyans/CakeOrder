2130238e584427c8525ff3e10c6cc752f622c74c
powderblue <zxp1937516045@163.com>
2022-11-08 18:47:53 +0800

    项目初始化

---
+++ pages/cake/cake.vue
@@ -0,0 +1,71 @@
+<template>
+	<view>
+		<view class="cont">
+			<view class="cake-item" v-for="(item,i) in 8" :key="i" @click="handleDetail(i)">
+				<view class="poster">
+					
+				</view>
+				<view class="info-count">
+					<view class="info">
+						<view class="fs-28">
+							草莓
+						</view>
+						<view class="fs-16">
+							QWERTR
+						</view>
+						<view class="fs-18">
+							<text class="fs-14">￥</text>
+							666.00
+						</view>
+					</view>
+					<view class="cart-btn">
+						
+					</view>
+				</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		},
+		methods:{
+			handleDetail(i){
+				console.log(i)
+				uni.navigateTo({
+					url:'/pages/detail/detail?i=' + i
+				})
+			}
+		}
+	}
+</script>
+
+<style lang="scss">
+	.cont{
+		display: flex;
+		flex-wrap:wrap;
+		padding: 15upx;
+		justify-content: space-between;
+	}
+.cake-item{
+	width: 350upx;
+	.poster{
+		height: 350upx;
+		background-color: #f5f5f5;
+	}
+	.fs-28{
+		margin-top: 24upx;
+	}
+	.fs-18{
+		margin-bottom: 22upx;
+	}
+	.fs-16{
+		margin: 14upx 0;
+	}
+}
+</style>
---
+++ pages/my/my.vue
@@ -0,0 +1,72 @@
+<template>
+	<view >
+		<view class="cont">
+			
+		</view>
+		<view class="margin bg-white">
+			<view class="text-center">
+				<image class="avatar" src="../../static/logo.png" mode=""></image>
+			</view>
+			<view class="fs-28 text-center">
+				QQ
+			</view>
+			<view v-for="(item,i) in 2" :key="i" class="flex grid">
+				<view v-for="(item,i) in gridArr" :key="i" class="flex flex-direction align-center padding">
+					<text :class="['iconfont', item.icon]"></text>
+					{{item.name}}
+				</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				gridArr:[
+					{
+						icon:'icon-danju',
+						name:'我的订单'
+						}
+					,
+					{
+						icon:'icon-jifen',
+						name:'待收货'
+					},
+					{
+						icon:'icon-ziliaoshouce',
+						name:'待付款'
+					},
+					{
+						icon:'icon-youhuijuan',
+						name:'会员等级'
+					}
+				]
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+page{
+	background-color: #fff;
+}
+.cont{
+	background-color: #f6f6f6;
+	height: 400upx;
+}
+.bg-white{
+	margin-top: -200upx;
+}
+.grid .iconfont{
+	font-size: 60upx;
+	color: #888;
+}
+.avatar{
+	width: 150upx;
+	height: 150upx;
+	border-radius: 50%;
+	margin-top: -75upx;
+}
+</style>
